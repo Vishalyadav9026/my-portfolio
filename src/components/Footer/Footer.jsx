356e270ffb02a30f8721b3d07e4c4ef9c9106317
Vishalyadav9026 <yadavvishalyadav718@gmail.com>
2024-01-01 18:37:15 +0530

    Lastest chnages

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -12,9 +12,15 @@ const Footer = () => {
       <div className="f-content">
         <span>yadavvishalyadav718@gmail.com</span>
         <div className="f-icons">
-          <Insta color="white" size={"3rem"} />
-          <Facebook color="white" size={"3rem"} />
-          <Gitub color="white" size={"3rem"} />
+        <a href="https://www.instagram.com/vishu_all_8578/" target="_blank" rel="noopener noreferrer">
+        <Insta color="white" size={"3rem"} />
+    </a>
+    <a href="https://www.facebook.com/people/Vishal-Yadav/pfbid021YqnahD7skWLEpXr5AH38s4GUgA7ncWCcPWvwn7yUjsESKvd6ufwa51FJDn3EYUol/?ref=xav_ig_profile_web" target="_blank" rel="noopener noreferrer">
+        <Facebook color="white" size={"3rem"} />
+    </a>
+    <a href="https://github.com/Vishalyadav9026" target="_blank" rel="noopener noreferrer">
+        <Gitub color="white" size={"3rem"} />
+    </a>
         </div>
       </div>
     </div>
